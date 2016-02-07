a089478e92ddfeffe2f83a5b14a41338b92392d3
spencer d <spencerkdavies@gmail.com>
2016-02-06 19:51:20 -0700

    u-spies - retabbed

--- z_upcase/spies/partial_double_spy_spec.rb
+++ z_upcase/spies/partial_double_spy_spec.rb
@@ -3,26 +3,26 @@
 require 'rspec'
 
 class Invitation
-	def self.deliver; end
-	def self.pulle; end	
+  def self.deliver; end
+  def self.pulle; end 
 end
 
 
 RSpec.describe "have_recieved" do 
-	it "passes when the message has been recieved" do 
-#		invitation = spy('invitation')
-		allow(Invitation).to receive(:pulle)
-#		invitation.deliver
-		Invitation.pulle
-#		expect(invitation).to have_received(:deliver)
-		expect(Invitation).to have_received(:pulle)
-	end
+  it "passes when the message has been recieved" do 
+#   invitation = spy('invitation')
+    allow(Invitation).to receive(:pulle)
+#   invitation.deliver
+    Invitation.pulle
+#   expect(invitation).to have_received(:deliver)
+    expect(Invitation).to have_received(:pulle)
+  end
 end
 =begin
 Rspec.describe "failure when the message has not been received" do
-	# you were just looking up example keyword to get
-	# an understanding of what it does
-	example "for a spy" do
+  # you were just looking up example keyword to get
+  # an understanding of what it does
+  example "for a spy" do
 
 end
 =end
