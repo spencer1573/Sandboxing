e15440719ae0549d0181ee145f61f8139ede6d15
spencer d <spencerkdavies@gmail.com>
2016-02-11 21:40:57 -0700

    making sure its updated

--- z_upcase/spies/partial_double_spy_spec.rb
+++ z_upcase/spies/partial_double_spy_spec.rb
@@ -24,20 +24,52 @@ RSpec.describe "failure when the message has not been received" do
   #so the 'example' keyword is possibly the worst word 
   # for rspec to have chosen for searchabilities sake. i still 
   # havn't figured out what it means and have searched and searched.
+=begin
   example "for a spy" do
     invitation = spy('iinvitation')
-    invitation.deliverr
+    #invitation.deliverr
     expect(invitation).to have_received(:deliverr)
   end
+=end
   
   example "testing for another thing" do 
     invitation = String.new
+<<<<<<< HEAD
     allow(invitation).to receive(:deliverrr)
     invitation.deliverrr
     expect(invitation).to have_received(:deliverrr)
   end
   
   example "for a partial double" do
+=======
+    # apparently this defines a 'hollow' method for the object
+    # without allow, to receive you get an error message
+    allow(invitation).to receive(:deliverrr)
+    invitation.deliverrr
+    expect(invitation).to have_received(:deliverrr)
+>>>>>>> tall-yew
   end
-
+  
 end
+
+RSpec.describe "An invitation" do
+end
+
+$count = 0
+#understanding let
+RSpec.describe "let" do
+  let(:count) { $count += 1 }
+
+  it "memorizes the value" do
+    expect(count).to eq(1)
+    expect(count).to eq(1)
+    $count = $count + 3
+    expect($count).to eq(4)
+  end
+  
+  it "is not cached across examples" do
+    expect($count).to eq(2)
+  end
+end
+
+
