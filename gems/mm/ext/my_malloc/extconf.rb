f7bd3776ce69a5fb1d04ab7c37c8e8ff6496b817
spencer d <spencerkdavies@gmail.com>
2016-02-22 21:16:50 -0700

    wrote extconf.rb studied create_makefile and added documentation about abort

---
+++ gems/mm/ext/my_malloc/extconf.rb
@@ -0,0 +1,13 @@
+require 'mkmf'
+
+# abort exits with a 1 and with a message
+#
+# it talks about it here:
+# http://www.sitepoint.com/exit-exit-abort-raise-get-me-outta-here/
+
+abort "missing malloc()" unless have_func "malloc"
+abort "missing free()"   unless have_func "free"
+
+create_makefile "my_malloc/my_malloc"
+
+
