f6a5b85fe8fa8340b4d72ea62a397fc6f46bb037
spencer d <spencerkdavies@gmail.com>
2016-02-24 23:08:37 -0700

    added a file to test with jshint to see how it changes it

---
+++ js/jshint_test.js
@@ -0,0 +1,17 @@
+
+// test
+console.log("something");
+
+myVar = 3;
+
+if (myVar == 1) {
+  console.log("it was 1");
+} else if (myVar == 2) {
+  console.log("it was 2");
+} else {
+  console.log("it doesn't matter");
+}
+
+
+
+
