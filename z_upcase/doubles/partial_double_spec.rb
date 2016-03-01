6769e95b63b5f896f40b4bc3c79ebce4ad564970
spencer d <spencerkdavies@gmail.com>
2016-03-01 13:52:29 -0700

    started working on partial doubles

---
+++ z_upcase/doubles/partial_double_spec.rb
@@ -0,0 +1,6 @@
+RSpec.describe "A partial double" do 
+  # "Note: stubbing a string like this is a terrible idea.
+  #       this is just for demonstration purposes
+  let(:string) { "a string" }
+
+end
