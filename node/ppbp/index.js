3bb98d752a28684470627910d0cb16271ed8be25
Spencer <snwmn24@gmail.com>
2016-03-21 15:18:51 -0600

    started pluralsight play by play tutorial

---
+++ node/ppbp/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+
+/* old node example
+var http = require('http');
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Hello Worlde\n');
+  //this is really nice for troubleshooting
+  //because it puts out an output anywhere during the code
+  //process.stdout.write("hwo: ");
+}).listen(3000, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:3000/');
+*/
