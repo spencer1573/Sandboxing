72c0e3b1f99f73dbf56402465cb09978ad1fa221
Spencer <snwmn24@gmail.com>
2016-03-23 22:43:56 -0600

    wrote hello world in express and used nodemon npm to get it to see the changes constantly.

--- node/ppbp/index.js
+++ node/ppbp/index.js
@@ -1,5 +1,33 @@
 var express = require('express');
 
+var app = express();
+
+app.get('/', function (req, res) {
+  //res.send('hello worlde');
+  res.json({hello: 'world'});
+});
+
+var server = app.listen(3000, function () {
+  console.log('Server running at http://127.0.0.1:3000/');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* old node example
 var http = require('http');
