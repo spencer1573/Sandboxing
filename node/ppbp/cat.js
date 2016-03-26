18d7da47a22bb41041a1fc7dd349a7a1c0cf35cd
Spencer <snwmn24@gmail.com>
2016-03-25 18:02:02 -0600

    added a new npm called lodash i think i will come to love it even though i have no idea what it is, also added a cat.js i have no idea what he's doing on the play by play but hopefully i'll figure it out.

---
+++ node/ppbp/cat.js
@@ -0,0 +1,28 @@
+var _ = require('lodash');
+
+module.exports = function(app) {
+
+    _cats = [];
+
+    /* Create */
+    app.post('/cat', function (req, res) {
+        _cats.push(req.body);
+        res.json({info: 'cat created successfully'});
+    });
+
+    /* Read */ 
+    app.get('/cat', function (req, res) {
+        res.send(_cats);
+    });
+
+    app.get('/cat/:id', function (req, res) {
+        res.send(
+            _.find(
+                _cats,
+                {
+                    name: req.params.id
+                }
+            )
+        );
+    });
+
