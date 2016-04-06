2387bd838ed7e6476aa798fb81063585e5d4f1e3
Spencer <snwmn24@gmail.com>
2016-04-06 17:28:47 -0600

    finished pets router for now so that it can display dogs server only

--- node/ppbp/pet_server.js
+++ node/ppbp/pet_server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-var petRoutes = require('./routes/pet.js')(app);
+var petRoutes = require('./routes/pets.js')(app);
 
 var server = app.listen(3002, function () {
   console.log('Server running at http://127.0.0.1:3002/');
