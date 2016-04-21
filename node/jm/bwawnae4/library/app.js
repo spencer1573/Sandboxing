f97274b76200481fb64bec404a2e0b66636b010a
Spencer <snwmn24@gmail.com>
2016-04-21 15:26:17 -0600

    added port and function callback to port 5000 for app.listen

--- node/jm/bwawnae4/library/app.js
+++ node/jm/bwawnae4/library/app.js
@@ -3,5 +3,9 @@ var express = require('express');
 // this is an instance of express
 var app = express();
 
-var port = 
-app.listen();
+var port = 5000;
+// the function is a callback that executes after it listens on port 5000
+app.listen(5000, function(err){
+
+});
+
