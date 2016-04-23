be839a017ca4cc4b1f460979d8fd5d9943df1324
Spencer <snwmn24@gmail.com>
2016-04-23 16:38:20 -0600

    made a app.get('/' and books it was cool

--- node/jm/bwawnae4/library/app.js
+++ node/jm/bwawnae4/library/app.js
@@ -3,6 +3,14 @@ var express = require('express');
 // this is an instance of express
 var app = express();
 
+app.get('/', function(req, res){
+    res.send('hello world')
+});
+
+app.get('/books', function(req, res) {
+    res.send('books!')
+});
+
 var port = 5000;
 // the function is a callback that executes after it listens on port 5000
 app.listen(port, function(err){
