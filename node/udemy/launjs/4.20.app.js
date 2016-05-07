e0fadc9e0ec20c6ea3f83f91e4b3765a6d56a02a
Spencer <snwmn24@gmail.com>
2016-05-06 20:11:14 -0600

    added more information about traking? or identifying prototypes using __proto__

--- node/udemy/launjs/4.20.app.js
+++ node/udemy/launjs/4.20.app.js
@@ -7,12 +7,27 @@ function Person(firstname, lastname) {
 }
 
 //prototype!
+/*
 Person.prototype.greet = function() {
     console.log("first name: " + this.firstname + " last name: " + this.lastname);
 }
+*/
+
+Person.prototype.number = 3;
+Person.prototype.string = "the string";
 
 var john = new Person('John', 'Doe');
 
 console.log(john.firstname);
 
-john.greet();
+//john.greet();
+
+// never use this in production, but what it does 
+// it lists ALL the prototypes and what they are
+// if its a function it doesn't write it all out 
+// it just says function. for number it says
+//
+// number: 3
+//
+// string: 'the string'
+console.log(john.__proto__);
