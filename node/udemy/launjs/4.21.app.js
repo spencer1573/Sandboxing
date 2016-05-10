5ca2d91929df134c21e89b91c5f0d797bb6fc883
Spencer <snwmn24@gmail.com>
2016-05-10 16:28:47 -0600

    made an illustration about a pass by refrence

--- node/udemy/launjs/4.21.app.js
+++ node/udemy/launjs/4.21.app.js
@@ -7,3 +7,26 @@ var a = 1;
 change(a);
 console.log(a);
 
+//pass by reference 
+// the reason that c changes after it
+// is called by the changeObj
+// is because c and d 
+// ARE BOTH POINTING TO THE SAME
+// OBJECT!!!
+//
+// this is super important for instance
+// when we require something
+// and need to change it everywhere
+// it changes it everywhere even outside 
+// of the function
+function changeObj(d) {
+    d.prop1 = function() {};
+    d.prop2 = {};
+}
+
+var c = {};
+c.prop1 = {};
+console.log(c);
+changeObj(c);
+console.log(c);
+
