e78ab1db72cdccde6a6ecd19caf2225f4bc8fff3
Spencer <snwmn24@gmail.com>
2016-05-12 17:14:55 -0600

    immediatly invoked a function by adding parentheses at the end of it. it was nuts

--- node/udemy/launjs/4.22.app.js
+++ node/udemy/launjs/4.22.app.js
@@ -1,4 +1,14 @@
 // notice that this function
 // has no NAME
-function () {
-}
+// this is an immediately invoked function
+// you just add parethesis on the end like
+// you do when you normally call a function
+(function () {
+
+    var firstname = 'John'; 
+    console.log(firstname);
+
+}()); 
+
+
+
