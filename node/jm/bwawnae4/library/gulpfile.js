36601e36b3952987432ee328396d3b561c100d2e
Spencer <snwmn24@gmail.com>
2016-05-12 17:09:54 -0600

    bwa - some gulp libraries

--- node/jm/bwawnae4/library/gulpfile.js
+++ node/jm/bwawnae4/library/gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
 
+var jshint = require('gulp-jshint');
+
 var jsFiles = ['*.js', 'src/**/*.js'];
 
-gulp.task('style', function(){
-    gulp.src();
+gulp.task('style', function() {
+    gulp.src(jsFiles)
+        .pipe(jshint());
 })
