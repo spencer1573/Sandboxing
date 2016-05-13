3663d9a71bd5ea0aa46fb28c3daa84ec9bb22568
Spencer <snwmn24@gmail.com>
2016-05-13 16:42:36 -0600

    learned abit about scope

--- node/udemy/launjs/4.22.app.js
+++ node/udemy/launjs/4.22.app.js
@@ -3,12 +3,18 @@
 // this is an immediately invoked function
 // you just add parethesis on the end like
 // you do when you normally call a function
-(function () {
+var firstname = 'Jane'; 
 
-    var firstname = 'John'; 
+(function () {
+    // in this case the firstname is accessing
+    // the global varaiable. you need to 
+    // put a var in front of it to declare it
+    // for this function's scope
+    firstname = 'John'; 
     console.log(firstname);
 
 }()); 
 
+console.log(firstname);
 
 
