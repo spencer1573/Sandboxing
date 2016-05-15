edf58ee206bfee1a02534c8fe882558ab8ef006e
Spencer <snwmn24@gmail.com>
2016-05-14 19:14:57 -0600

    added documentation about how to get gulp to work on the command line with command line tools

--- node/jm/bwawnae4/library/gulpfile.js
+++ node/jm/bwawnae4/library/gulpfile.js
@@ -4,10 +4,14 @@ var jshint = require('gulp-jshint');
 
 var jsFiles = ['*.js', 'src/**/*.js'];
 
+// to get gulp to work in the command line you need to remember to 
+// install gulp cl tools
+// by:
+// npm install -g gulp-cli
 gulp.task('style', function() {
     gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
             verbose: true
-        }))
+        }));
 })
