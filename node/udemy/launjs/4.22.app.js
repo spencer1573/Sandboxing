5a59f162d37bfa63bd44581fda56e8c14fd6cbbe
Spencer <snwmn24@gmail.com>
2016-05-14 19:22:51 -0600

    lau - you can even pass in stuff as you instantiate it its pretty crazy.

--- node/udemy/launjs/4.22.app.js
+++ node/udemy/launjs/4.22.app.js
@@ -5,15 +5,16 @@
 // you do when you normally call a function
 var firstname = 'Jane'; 
 
-(function () {
+(function (lastname) {
     // in this case the firstname is accessing
     // the global varaiable. you need to 
     // put a var in front of it to declare it
     // for this function's scope
     firstname = 'John'; 
     console.log(firstname);
+    console.log(lastname);
 
-}()); 
+}('Doe')); 
 
 console.log(firstname);
 
