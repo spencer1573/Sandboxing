5cc46e8108343458369c75f10b5731bdcb507dec
Spencer <snwmn24@gmail.com>
2016-05-16 12:52:16 -0600

    added gulp and jshint to the things that come up in the command line when you run gulp style

--- node/jm/bwawnae4/library/gulpfile.js
+++ node/jm/bwawnae4/library/gulpfile.js
@@ -2,16 +2,22 @@ var gulp = require('gulp');
 
 var jshint = require('gulp-jshint');
 
+var jscs = require('gulp-jscs');
+
 var jsFiles = ['*.js', 'src/**/*.js'];
 
 // to get gulp to work in the command line you need to remember to 
 // install gulp cl tools
 // by:
 // npm install -g gulp-cli
+//
+// you are at 6:20 in the video right now
+//
 gulp.task('style', function() {
-    gulp.src(jsFiles)
+    return gulp.src(jsFiles)
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish', {
             verbose: true
-        }));
+        }))
+        .pipe(jscs());
 })
