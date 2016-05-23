2e0ba1e1d7036546e7e382978b06ae5216e787ca
Spencer <snwmn24@gmail.com>
2016-05-23 01:53:04 -0600

    cant figure out when i ignore the path and then run gulp inject if i add that "c" to that public for some reason it deletes it when i gulp inject it... can't figure out why

--- node/jm/bwawnae4/library/gulpfile.js
+++ node/jm/bwawnae4/library/gulpfile.js
@@ -24,7 +24,8 @@ gulp.task('inject', function() {
     var wiredep = require('wiredep').stream;
     var options = {
         bowerJson: require('./bower.json'),
-        directory: './public/lib'
+        directory: './public/lib',
+        ignorePath: '../../publi'
     };
 
     return gulp.src('./src/views/*.html')
