a228bf212d97d95a95994bde5ba5fbf476393cd2
Spencer <snwmn24@gmail.com>
2016-06-13 15:16:15 -0600

    angular2 heros tut - 4. Multiple Components completed

--- node/S_en/angular-heroes/app/app.component.ts
+++ node/S_en/angular-heroes/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from './hero';
+
 
 export class Hero {
   id: number;
@@ -9,26 +12,16 @@ export class Hero {
   selector: 'my-app',
   template:`
     <h1>{{title}}</h1>
-    
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" 
+      <li *ngFor="let hero of heroes"
         [class.selected]="hero === selectedHero"
         (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-      </div>
-    </div>
-    `
-
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
   styles:[`
     .selected {
       background-color: #CFD8DC !important;
@@ -77,7 +70,8 @@ export class Hero {
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  directives: [HeroDetailComponent]
 })
 
 export class AppComponent {
