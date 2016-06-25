54a6095b781c6500f6c4882ee6f51c5ab967b881
Spencer Davies <snwmn24@gmail.com>
2016-06-24 18:57:03 -0600

    modified test app

--- node/test/app.js
+++ node/test/app.js
@@ -2,14 +2,6 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 
-var app = express();
-
-// use app.router before express.static
-//depreciated in express 4.0
-//app.use(app.router);
-app.use(express.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
 function userIsAllowed(callback) {
   // this function would contain your logic, presumably asynchronous,
   // about whether or not the user is allowed to see files in the
@@ -17,22 +9,31 @@ function userIsAllowed(callback) {
   callback(false);
 }
 
+// This function returns a middleware function
+var protectPath = function(regex) {
+  return function(req, res, next) {
+    if (!regex.test(req.url)) { return next(); }
+
+    userIsAllowed(function(allowed) {
+      if (allowed) {
+        next(); // send the request to the next handler, which is express.static
+      } else {
+        res.end('You are not allowed!');
+      }
+    });
+  };
+};
+
+var app = express();
+
+app.use(protectPath(/^\/protected\/.*$/));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res, next) {
   res.end('Home page');
-});
-
-app.get('/protected/*', function(req, res, next) {
-  userIsAllowed(function(allowed) {
-    if (allowed) {
-      next(); // call the next handler, which in this case is express.static
-    } else {
-      res.end('You are not allowed!');
-    }
-  });
 });
 
 http.createServer(app).listen(3000, function(){
   console.log('Express server listening on port 3000');
 });
-
 
