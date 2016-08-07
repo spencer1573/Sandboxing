8c8d9cc256573c24e6791d9e0cf98153dd0d750c
Spencer Davies <snwmn24@gmail.com>
2016-08-06 23:19:00 -0600

    added starting point to 0.0.3 prime problem en

---
+++ node/en_assign/0.0.3-check-if-prime/app.js
@@ -0,0 +1,14 @@
+/*
+Check to see if the number passed into your function is prime or not.  77 is not prime and 13 is prime.
+
+ checkIfPrime(77);  <-make sure to pass the Number into your function, so the code is DRY (reusable).
+
+  Submit your .js/ts file on completion.
+*/
+
+
+function checkifPrime(number) {
+
+}
+
+checkifPrime(77);
