197ea34b49fb69ff208b69f32819f9c60c274d18
Spencer Davies <snwmn24@gmail.com>
2016-08-09 22:26:38 -0600

    solved 0.0.3 prime number dev engage practice

--- node/en_assign/0.0.3-check-if-prime/app.js
+++ node/en_assign/0.0.3-check-if-prime/app.js
@@ -10,9 +10,33 @@ Check to see if the number passed into your function is prime or not.  77 is not
 function checkifPrime(number) {
 
   if (number > 1) { 
-    
-      
+
+    var isPrime = true;
+    for (var i = number - 1; i > 1; i--) {
+      //console.log(i + " _ " + number + ": " + number % i);
+
+      if ((number % i) === 0) {
+        isPrime = false;
+      }
+
+    }
+     
+    var isOrIsNot = "";
+
+    if (isPrime) {
+      //do nothing
+    } else {
+      isOrIsNot = "not ";
+    }
+    var answer = number + " is " + isOrIsNot + "prime"; 
+
+    console.log(answer);
+
+    return answer;
   } 
 }
 
+checkifPrime(2);
+checkifPrime(6);
 checkifPrime(77);
+checkifPrime(17);
