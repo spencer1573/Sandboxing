826283a265fc7be31513b0db87aa4460d6b5a552
Spencer <snwmn24@gmail.com>
2016-09-05 15:47:24 -0600

    added a primeng quickstart exmaple

---
+++ primeng-quickstart/app/app.module.ts
@@ -0,0 +1,16 @@
+import {NgModule}      from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule}    from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent}  from './app.component';
+import {CarService} from './cars/carservice';
+import {InputTextModule,DataTableModule,ButtonModule,DialogModule} from 'primeng/primeng';
+
+@NgModule({
+  imports:      [BrowserModule,FormsModule,HttpModule,InputTextModule,DataTableModule,ButtonModule,DialogModule],
+  declarations: [AppComponent],
+  bootstrap:    [AppComponent],
+  providers:    [CarService]
+})
+export class AppModule { }
