fe607b8c1cd6f8d29816ea79f057d22a418e5861
Spencer <snwmn24@gmail.com>
2016-09-07 14:52:28 -0600

    added java example, simple app, primeng and lightning voter demo

---
+++ ps-java/16.09.06.organized/src/com/pluralsight/getorganized/Main.java
@@ -0,0 +1,10 @@
+package com.pluralsight.getorganized;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello intelliJ");
+	// write your code here
+    }
+}
