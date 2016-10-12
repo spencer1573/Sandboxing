ec21f6c023a0342c79768b61d18c8db09700cea0
Spencer Davies <snwmn24@gmail.com>
2016-10-11 23:42:12 -0600

    need to learn how to return time still

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -6,27 +6,35 @@ import (
 )
 
 func addHour() {
+        
 }
 /*
 func addZeros(time int) {
         fmt.Printf("%d \n", )
 }
 */
-
+/*
 func printTime(hour int, min int) int{
         //fmt.Printf("%d:%d \n", hour.Hour(), min)
         p := fmt.Println
 
         now := time.Now()
         p(now)
-        then := time.Date(
-                2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
         //oneHour := now.Date()
         p(then)
         //var theMin int64 =  2
         //var convertMin int64 = theMin * 100000000000
         p(then.Add(3 * time.Second ))
         return 0
+}
+*/
+func clockInit() int {
+        p := fmt.Println
+        clock := time.Date(
+                2009, 11, 17, 20, 34, 58, 651387237, time.UTC)
+        p(clock)
+        //return clock
+        return 5
 }
 
 func menu() {
@@ -40,9 +48,10 @@ func main() {
         fmt.Printf("\nThis Program adds minutes and hours to clock\n\n")
         fmt.Printf("These are your options:\n\n")
         menu()
-        var hour int = 9
-        var min int = 0
-        printTime(hour, min)
+        //var hour int = 9
+        //var min int = 0
+        clockInit()
+        //printTime(hour, min)
         /*
         n, err := fmt.Scanf("%d\n", &year)
         if err != nil || n != 1 {
