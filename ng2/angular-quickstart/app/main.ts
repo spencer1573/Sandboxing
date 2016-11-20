2660fecdc394aca3b042ea71f087e92732fa9ca3
spencer1573 <spencer1573@users.noreply.github.com>
2016-11-19 20:53:20 -0700

    app is up and running

--- ng2/angular-quickstart/app/main.ts
+++ ng2/angular-quickstart/app/main.ts
@@ -1,4 +1,4 @@
-import { platformBrowserDynamic } from '@angular/platformBrowserDynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
