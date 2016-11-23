84fbf3d6da4a0e11f35272a00348fb1ba0fee013
spencer1573 <spencer1573@users.noreply.github.com>
2016-11-22 19:03:21 -0700

    initialized properties in the constructor

--- ng2/displaying-data/app/app.component.ts
+++ ng2/displaying-data/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+    template: `
+      <h1>{{title}}</h1>
+      <h2>My favorite hero is: {{myHero}}</h2>
+    `
 })
 export class AppComponent {
-    title = 'Tour of Heroes';
-    hero = 'Windstorm';
+    title: string;
+    myHero: string;
+
+    constructor() {
+      this.title = 'Tour of Heroes';
+      this.myHero = 'Windstorm';
+    }
 }
