e3e4a2e7a13a5aaf2f3e08b0dd97e9aaa74e61be
spencer1573 <spencer1573@users.noreply.github.com>
2016-11-26 23:13:57 -0700

    unsure of what i did, but things look good

--- ng2/displaying-data/app/app.component.ts
+++ ng2/displaying-data/app/app.component.ts
@@ -5,14 +5,28 @@ import { Component } from '@angular/core';
     template: `
       <h1>{{title}}</h1>
       <h2>My favorite hero is: {{myHero}}</h2>
+      <ol>
+        <li *ngFor="let hero of heroes">
+          {{ hero }}
+        </li>
+      <ol>
     `
 })
 export class AppComponent {
     title: string;
     myHero: string;
+    heroes: Array<string> = [];
 
     constructor() {
       this.title = 'Tour of Heroes';
-      this.myHero = 'Windstorm';
+      this.heroes = [
+        'windstorm',
+        'wolverine',
+        'charles xavior',
+        'magenta',
+        'tornado'
+      ];
+      this.myHero = this.heroes[0];
+
     }
 }
