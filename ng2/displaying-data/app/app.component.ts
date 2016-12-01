64793d5656e8f4f5d9db37b77c12c47998240118
spencer1573 <spencer1573@users.noreply.github.com>
2016-11-30 22:57:00 -0700

    added ability to tell if there were more than three heroes inthe list. i believe i also set it up to display the first hero... the long way... by key a few days ago

--- ng2/displaying-data/app/app.component.ts
+++ ng2/displaying-data/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
     selector: 'my-app',
@@ -7,26 +8,31 @@ import { Component } from '@angular/core';
       <h2>My favorite hero is: {{myHero}}</h2>
       <ol>
         <li *ngFor="let hero of heroes">
-          {{ hero }}
+          {{ hero.name }}
         </li>
       <ol>
+      <p *ngIf="heroes.length > 3">there are many heroes!</p>
     `
 })
 export class AppComponent {
     title: string;
     myHero: string;
-    heroes: Array<string> = [];
+    //heroes: Array<number|string> = [];
+    heroes: Array<Object> = [];
 
     constructor() {
       this.title = 'Tour of Heroes';
+      console.log(new Hero(1, 'windstorm'));
       this.heroes = [
-        'windstorm',
-        'wolverine',
-        'charles xavior',
-        'magenta',
-        'tornado'
+        new Hero(1, 'windstorm'),
+        new Hero(13, 'Bombasto'),
+        new Hero(15, 'Magenta'),
+        new Hero(20, 'Tornado'),
+        new Hero(21, 'Wolverine'),
+        new Hero(21, 'Charles xavior'),
+        new Hero(31, 'Magenta')
       ];
-      this.myHero = this.heroes[0];
+      this.myHero = this.heroes[Object.keys(this.heroes)[0]];
 
     }
 }
